e797e1a7ab121cc40d81f36d876f75ff42e7ece0
Márcio Serafim <marciofilho2@hotmail.com>
2019-04-09 08:16:08 -0300

    arrumando a criação de estoque

--- View/Pages/Empresa/CreateEstoque.cshtml.cs
+++ View/Pages/Empresa/CreateEstoque.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Modelo;
 using Persistencia;
 
@@ -19,15 +20,17 @@ namespace View.Pages.Empresa
             _context = context;
         }
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGet(int? idEmpresa)
         {
-        ViewData["IdEmpresa"] = new SelectList(_context.Empresa, "Id", "NomeEmpresa");
-        ViewData["IdProduto"] = new SelectList(_context.Produto, "Id", "NomeProduto");
+            ViewData["IdEmpresa"] = new SelectList(_context.Empresa, "Id", "NomeEmpresa");
+            ViewData["IdProduto"] = new SelectList(_context.Produto, "Id", "NomeProduto");
+            Empresa = await _context.Empresa.FirstOrDefaultAsync(x => x.Id == idEmpresa);
             return Page();
         }
 
         [BindProperty]
         public ProdutoEstoque ProdutoEstoque { get; set; }
+        public static Modelo.Empresa Empresa { get; set; }
 
         public async Task<IActionResult> OnPostAsync()
         {
@@ -36,7 +39,24 @@ namespace View.Pages.Empresa
                 return Page();
             }
 
-            _context.ProdutoEstoque.Add(ProdutoEstoque);
+            var estoque = await _context.ProdutoEstoque.FirstOrDefaultAsync(x => x.IdProduto == ProdutoEstoque.IdProduto && x.IdEmpresa == Empresa.Id);
+            if (estoque != null)
+            {
+                estoque.Estoque += ProdutoEstoque.Estoque;
+                _context.Attach(estoque).State = EntityState.Modified;
+                _context.ProdutoEstoque.Update(estoque);
+            }
+            else
+            {
+                _context.ProdutoEstoque.Add(ProdutoEstoque);
+            }
+            _context.ProdutoMovimentacao.Add(new ProdutoMovimentacao
+            {
+                Data = DateTime.Now,
+                IdEmpresa = Empresa.Id,
+                IdProduto = ProdutoEstoque.IdProduto,
+                Movimentacao = ProdutoEstoque.Estoque
+            });
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
